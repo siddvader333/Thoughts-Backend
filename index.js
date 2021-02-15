a8e89154b2ebff2e085287ccb8e4ae7d5ef5af68
Siddharth <siddharth.vjay333@gmail.com>
2021-02-14 22:05:56 -0500

    Server for thoughts -chrome extension with socket.io

---
+++ index.js
@@ -0,0 +1,38 @@
+// express
+var express = require('express');
+var app = express();
+
+// socket.io
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// turn off unnecessary header
+app.disable('x-powered-by');
+
+// turn on strict routing
+app.enable('strict routing');
+
+// use the X-Forwarded-* headers
+app.enable('trust proxy');
+
+// add CORS headers
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
+io.on('connection', function(socket) {
+	console.log('connected');
+
+	socket.on('send-thought', (data) => {
+		console.log('send-thought received');
+		console.log(data);
+		socket.emit('receive-thought', data);
+	});
+});
+
+var server = http.listen(process.env.PORT || 3000, function() {
+	console.log('Listening on port %d.', server.address().port);
+});
